4ce886756933494ef27176bb7d7c2c9c942ec082
Ryan Arthur <ryan@gaslight.co>
2017-01-18 15:58:48 -0500

    Add ES6

--- Gemfile
+++ Gemfile
@@ -23,6 +23,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+#ES6
+gem 'sprockets', '>=3.0.0.beta'
+gem 'sprockets-es6'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+require 'sprockets/es6'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
